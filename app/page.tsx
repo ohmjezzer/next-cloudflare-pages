2f425f8a450da6658b8cafc2640e740872993274
jessada <jessada@myorder.ai>
2025-12-06 23:42:55 +0700

    test: feat 1

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h1>Home</h1>
-      <p>Hello World! This is the Home page from feat 1</p>
+      <p>Hello World! This is the Home page from feat 1 and new push</p>
       <p>
         Visit the <Link href="/about">About</Link> page.
       </p>
